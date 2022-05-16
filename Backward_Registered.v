23bcc269b5f889aa83a144d2bd5ba8c9e7ab3b94
Brandon <75628994+BrandondonChi@users.noreply.github.com>
2022-05-17 00:29:47 +0800

    Add files via upload

--- Backward_Registered.v
+++ Backward_Registered.v
@@ -1,22 +1,95 @@
 `timescale 1ns / 1ps
-module Backward_Registered #( parameter WIDTH = 8) (
+
+module Backward #( parameter DWIDTH = 8 ) 
+(
+   input  clk     ,    
+   input  rst_n   ,   
+   
+   input   [DWIDTH-1 : 0] m_data   ,     
+   input                  m_valid  ,     
+   output                 m_ready  ,     
+
+   output  [DWIDTH-1 : 0] s_data   ,     
+   output                 s_valid  ,     
+   input                  s_ready        
+) ;
+
+parameter S0  = 1'b0 ;
+parameter S1  = 1'b1 ;
+
+reg                state                                     ;        
+reg [DWIDTH-1 : 0] data_rg, sparebuff_rg                     ;        
+reg                valid_rg, sparebuff_valid_rg, ready_rg    ;        
+wire               ready                                     ;        
+
+always @(posedge clk) begin 
+   if (!rst_n) begin
+      state              <= S0 ;
+      data_rg            <= '0   ;     
+      sparebuff_rg       <= '0   ;
+      valid_rg           <= 1'b0 ;
+      sparebuff_valid_rg <= 1'b0 ;
+      ready_rg           <= 1'b0 ;
+
+   end
+   else begin
+      case (state)   
+         S0 : begin          
+            if (ready) begin
+               data_rg            <= m_data  ;  
+               valid_rg           <= m_valid ;
+               ready_rg           <= 1'b1    ;               
+            end
+            else begin
+               sparebuff_rg       <= m_data  ;
+               sparebuff_valid_rg <= m_valid ;
+               ready_rg           <= 1'b0    ;
+               state           <= S1    ;
+            end
+
+         end
+         S1 : begin       
+            if (ready) begin
+               data_rg  <= sparebuff_rg       ;
+               valid_rg <= sparebuff_valid_rg ;               
+               ready_rg <= 1'b1               ;
+               state <= S0                    ;               
+            end
+
+         end
+
+      endcase
+
+   end
+
+end
+
+assign ready   = s_ready || ~valid_rg ;
+assign m_ready = ready_rg             ;
+assign s_data  = data_rg              ;
+assign s_valid = valid_rg             ;
+
+endmodule
+
+/*
+module Backward #( parameter WIDTH = 8) (
         input              clk,
         input              rst_n,
 
         input              m_valid,
         output             m_ready,
-        input [WIDTH-1:0] m_data,
+        input [WIDTH-1:0]  m_data,
 
         output             s_valid,
         input              s_ready,
-        output [WIDTH-1:0] s_data,
+        output [WIDTH-1:0] s_data
         );
 
     reg                        reg_valid;
     wire                       reg_valid_in;
     reg                        reg_ready_out;
     wire                       reg_ready_in;
-    wire                       reg_no_data_in;
+    wire                       reg_ns_data_in;
     wire                       pass_through;
     wire                       reg_valid_out;
     reg    [0:WIDTH-1]         reg_data;
@@ -32,7 +105,7 @@ always @(posedge clk)begin
     end
     else
     begin
-        reg_ready_out <= (reg_ready_in||(reg_no_data_in && ~reg_valid));//除气泡 数据存入寄存器且寄存器内无数据时，m_ready可为高
+        reg_ready_out <= (reg_ready_in||(reg_ns_data_in && ~reg_valid));//除气泡 数据存入寄存器且寄存器内无数据时，m_ready可为高
         if (reg_ready_out)//寄存器准备好接收数据
         begin
             reg_valid <= reg_valid_in;
@@ -49,7 +122,7 @@ end
 
 
 
-assign reg_no_data_in = ~(reg_valid_in && reg_ready_out);//master握手不成功时为1，此时无数据进入寄存器
+assign reg_ns_data_in = ~(reg_valid_in && reg_ready_out);//master握手不成功时为1，此时无数据进入寄存器
 
 assign pass_through = m_ready && s_ready && ~reg_valid;//数据直通状态 ready一直为高时，只需valid为高即可直接进行握手
 
@@ -66,3 +139,4 @@ assign s_valid = pass_through ? m_valid : reg_valid;
 assign s_data = pass_through ? m_data : reg_data;
 
 endmodule
+*/
